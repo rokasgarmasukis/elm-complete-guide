8443944d54e84699cc3b1f370342c8738fbdb73f
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-03-30 08:59:14 +0200

    Finish lesson

--- 04-happy-birthday/src/Main.elm
+++ 04-happy-birthday/src/Main.elm
@@ -1,35 +1,79 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (..)
-import Html.Events exposing (onClick)
+import Html
+import Html.Attributes exposing (value)
+import Html.Events exposing (onClick, onInput)
 
 
+initModel : Model
+initModel =
+    { message = "Welcome"
+    , firstname = ""
+    , age = 0
+    }
+
+
+main : Program () Model Msg
 main =
     Browser.sandbox
-        { init = "Welcome"
+        { init = initModel
         , view = view
         , update = update
         }
 
 
 type Msg
-    = Surprise
-    | Reset
+    = MsgSuprise
+    | MsgReset
+    | MsgNewName String
+    | MsgNewAgeAsString String
 
 
-view data =
-    div []
-        [ text (data ++ " ")
-        , button [ onClick Surprise ] [ text "Surprise" ]
-        , button [ onClick Reset ] [ text "Reset" ]
+type alias Model =
+    { message : String
+    , firstname : String
+    , age: Int
+    }
+
+
+view : Model -> Html.Html Msg
+view model =
+    Html.div []
+        [ Html.text model.message
+        , Html.input [ onInput MsgNewName, value model.firstname ] []
+        , Html.input [ onInput MsgNewAgeAsString, value (String.fromInt model.age) ] []
+        , Html.button [ onClick MsgSuprise ] [ Html.text "Surprise" ]
+        , Html.button [ onClick MsgReset ] [ Html.text "Reset" ]
+        , Html.text (String.fromInt (String.length model.firstname))
         ]
 
 
-update msg data =
+update : Msg -> Model -> Model
+update msg model =
     case msg of
-        Surprise ->
-            "Happy birthday!"
+        MsgSuprise ->
+            { model
+                | message = "Happy Birthday " ++ model.firstname ++ " with " ++ (String.fromInt model.age) ++ " years old !!"
+            }
 
-        Reset ->
-            "Welcome"
+        MsgReset ->
+            initModel
+
+        MsgNewName newName ->
+            { model
+                | firstname = newName
+            }
+        
+        MsgNewAgeAsString newValue ->
+            case String.toInt newValue of
+                Just anInt ->
+                    {
+                        model
+                            | age = anInt
+                    }
+                Nothing ->
+                    {
+                        model
+                            | message = "The age is wrong", age = 0
+                    }
